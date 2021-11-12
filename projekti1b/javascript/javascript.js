3bc72cd82fe2e4774d3a8ff0121e9576354d266f
Hexulez <henry.juhola@edu.keuda.fi>
2021-11-12 17:58:05 +0200

    Väliaika javascript

--- projekti1b/javascript/javascript.js
+++ projekti1b/javascript/javascript.js
@@ -36,3 +36,31 @@ form.ika.focus();
 return false;
 }
 }
+
+
+// En ole vielä tarkistanut toimiiko nämä kuten suunnittelin,
+//mutta ovat kuitenkin varmaan aika lähelle oikeaa
+function droppi() {
+  let satunnainenMuuttujaNimi = document.getElementById("pudotus").value;
+  //let muuttujalla mun muuttuja ei toimi tämän funktion ulkopuolella
+  if (satunnainenMuuttujaNimi == "") { //tarkistan onko muuttujan arvo tyhjä
+    alert("Valitse jonkin vaihtoehto pudotus valikosta");
+    return false; //palauttaa arvon epätosi eli false
+}
+  else { //jos edellinen if lauseke täyty tulee toteuttaa tämän.
+    return true; // palauttaa arvon totta eli true.
+  }
+  }
+function radiot() {
+  let satunnainenMuuttujaNimi = document.getElementById("")
+}
+
+
+
+
+  function nappula1() {
+    let drop = droppi() // tekee mutttujan drop ja ottaa joko false tai true arvon functiolta droppi.
+    if (drop === true) {
+      alert("Kaikki on oikein")
+    }
+  }
