7fce3e56308ea3c52d3a39bb9a9b7c2017cfaf15
TiinaKop <tiinaKop>
2021-11-12 16:23:50 +0200

    scriptiä

--- projekti1b/javascript/javascript.js
+++ projekti1b/javascript/javascript.js
@@ -1,4 +1,4 @@
-function Tulosta() {
+function Tarkasta() {
 
 var nimi = document.getElementById('nimi').value;
 
@@ -6,6 +6,33 @@ if(nimi.length < 2)
 {
 alert("Et antanut nimeäsi");
 form.nimi.focus();
+return false;
+}
+}
+
+function Tarkasta() {
+
+var email = document.getElementById('email').value;
+
+function emailIsValid (email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) }
+  if(emailIsValid(sahkoposti)) {
+    // else?
+    alert("Anna oikea sähköpostiosoitteesi");
+  form.email.focus();
+  return (false);
+}
+}
+
+
+function Tarkasta() {
+
+var ika = document.getElementById('ika').value;
+
+if(ika.length < 2)
+{
+alert("Et antanut nimeäsi");
+form.ika.focus();
 return false;
 }
 }
