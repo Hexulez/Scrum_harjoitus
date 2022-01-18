374c42b18de62a3d9eaf20c557b060bcc2870dcc
Hexulez <henry.juhola@edu.keuda.fi>
2022-01-18 14:12:23 +0200

    Update js littlebit and make test temp file

--- projekti5/javascript/muistipeli.js
+++ projekti5/javascript/muistipeli.js
@@ -1,17 +1,21 @@
 function valitse() { //funktio jolla voi kutsua muut osat
   let cards = document.getElementById("valitseKoko").value;
   console.log(cards);
-  pelilauta(cards);
+  pelilauta(parseInt(cards));
 }
 
+// boardEraser I function what check is there any div class"peli" and delete those.
+boardEraser = () =>{
+  let div = document.querySelector('.peli');
+  console.log(div);
+  if (div != null ) {
+    console.log("porn");
+    document.querySelectorAll('.peli').forEach(e => e.remove());
+  }}
+// boardEraser end here
 
-//Tästä olisi tarkoitus rakentaa funktio joka lisää ja poistaa div elementejä
-//sen mukaan kuinka paljon haluaa käyttää kortteja
-function pelilauta(cards) {
-  var elem = document.getElementsByClassName("peli");
-  elem.parentNode.removeChild(elem);
-  //tähän pitäisi lisätä osio mikä tarkistaa onko niitä vielä ja poistaa vanhat
-  
+// makeGameBoard is function and this make right number of cards what needed
+makeGameBoard = (cards) =>{
   for (let i = 0; i < cards; i++) { //tämä osio lisää niitä
     const divine = document.createElement('div');
     const teksti = document.createTextNode("testi teksti.");
@@ -20,10 +24,16 @@ function pelilauta(cards) {
     console.log(i);
     divine.appendChild(teksti);
     element.appendChild(divine);
-    //let testi = document.getElementsByTagName("DIV").className;
+    let testi = document.getElementsByTagName("DIV").className;
     //testi.classList.add("peli")
 
-  }
+  }}
+//makeGameBoard end here
+
+
+function pelilauta(cards) {
+  boardEraser();
+  makeGameBoard(cards);
 
 //  for (let j = 0; j < cards; j--) {
   //  const divi = document.deleteElement('div');
